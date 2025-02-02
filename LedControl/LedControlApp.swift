2921e48c8ccae4151498573bb16abafed234431b
Mihai Gusa <mihaigusa90@gmail.com>
2025-02-02 22:14:01 +0200

    Initial Commit

---
+++ LedControl/LedControlApp.swift
@@ -0,0 +1,32 @@
+//
+//  LedControlApp.swift
+//  LedControl
+//
+//  Created by Mihai on 02.02.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LedControlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
